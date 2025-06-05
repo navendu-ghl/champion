6af02daecad37d22b5738393824724c66ea1ef06
Devangi Naliyadhara <devangi.naliyadhara@Devangis-MacBook-Pro.local>
2025-06-05 17:08:55 +0530

    team and mode in slack payload

--- index.js
+++ index.js
@@ -80,18 +80,20 @@ async function handleGeneralAutomation(req, res) {
 async function handleSlackInteraction(req, res) {
     const payload = JSON.parse(req.body.payload);
     const action = payload.actions?.[0];
-    const userId = payload.user.id;
-    const team = Object.entries(slackData).find(([_, value]) => value.reviewer === userId)[0];
     const actionId = action?.action_id;
+    const valuePayload = JSON.parse(action?.value);
+    const mode = valuePayload.mode;
+    const userId = payload.user.id;
+    const team = valuePayload.team || Object.entries(slackData).find(([_, value]) => value.reviewer === userId)[0];
 
     switch (actionId) {
         case 're-generate-release-digest':
             console.log("Re-generating release digest");
-            handleGeneralAutomation({ query: { action: 'post-weekly-release-digest', mode: 'review', team } });
+            handleGeneralAutomation({ query: { action: 'post-weekly-release-digest', mode: mode || 'review', team } });
             break;
         case 'publish-release-digest':
             console.log("Publishing release digest");
-            handleGeneralAutomation({ query: { action: 'post-weekly-release-digest', mode: 'publish', team } });
+            handleGeneralAutomation({ query: { action: 'post-weekly-release-digest', mode: moode || 'publish', team } });
             break;
     }
 
